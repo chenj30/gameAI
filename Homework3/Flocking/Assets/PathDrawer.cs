6d19e3cea95b7b172f219748d43410e934ebe750
JCHEN-Y410P\Johnny <johnnysanchen@gmail.com>
2016-10-05 03:35:21 -0400

    Attempted flocking hw

---
+++ Homework3/Flocking/Assets/PathDrawer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class PathDrawer : MonoBehaviour {
+
+	public Transform[] nodes;
+	private LineRenderer lineRender;
+
+	void CreateNodes()
+	{
+		for (int i = 0; i < nodes.Length; i++)
+		{
+			Vector3 position = new Vector3(nodes[i].position.x, nodes[i].position.y, 0);
+			lineRender.SetPosition(i, position);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		lineRender = gameObject.GetComponent<LineRenderer>();
+		lineRender.SetVertexCount(nodes.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CreateNodes();
+	}
+}
