bbe27e9df26f095585c3256ef45c78f059140999
JCHEN-Y410P\Johnny <johnnysanchen@gmail.com>
2016-10-13 16:06:17 -0400

    -made workbenches for everyone -added buttons for switch formations -reorganized assests

---
+++ Homework4/MultiAgentMovement/Assets/Scripts/FormationSwitch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FormationSwitch : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject birdLeader;
+
+	private Text _txtObj;
+
+	// Use this for initialization
+	void Start () {
+		_txtObj = this.gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoToScalable()
+	{
+		_txtObj.text = "Scalable Formation";
+		// change formation to scalable
+	}
+
+	public void GoToEmerged()
+	{
+		_txtObj.text = "Emerged Formation";
+		// change formation to emgered 
+	}
+
+	public void GoToTwoLevel()
+	{
+		_txtObj.text = "Two-Level Formation";
+		// change formation to two-level
+	}
+
+}
