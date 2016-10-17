4b02de8b64b865c889d74450205d1d16105e52bf
Andy <awong7133@gmail.com>
2016-10-17 18:26:21 -0400

    Attempt at emergence

---
+++ Homework4/MultiAgentMovement/Assets/Scripts/Andy_TestScripts/FollowMouse_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse_Script : MonoBehaviour {
+
+	// PUBLIC VARIABLES
+
+	#region Unity functions
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)) {
+			Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			mousePosition.z = 5f;
+
+			float angle = Mathf.Atan2 (mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+
+			transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+			transform.position = Vector3.MoveTowards(transform.position, mousePosition, 4 * Time.deltaTime);
+		}
+	}
+	#endregion
+}
